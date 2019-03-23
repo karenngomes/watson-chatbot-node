70c783fea8a69e51ec22a21a14201b36947ce42c
Karen Gomes <kngs@ic.ufal.br>
2019-03-23 19:32:47 -0300

    feat(app.js): add watson assistant

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const AssistantV1 = require("watson-developer-cloud/assistant/v1");
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -6,10 +8,30 @@ app.use(bodyParser.json());
 
 const port = 3000;
 
-app.get("/conversation/:text*?", (req, res) => {
-  const { text } = req.params;
+const assistant = new AssistantV1({
+  username: process.env.ASSISTANT_USERNAME,
+  password: process.env.ASSISTANT_PASSWORD,
+  url: "https://gateway-wdc.watsonplatform.net/assistant/api",
+  version: "2019-02-23",
+  
+});
 
-  res.json(text);
+app.post("/conversation/", (req, res) => {
+  const { text, context = {} } = req.params;
+  const params = {
+    input: { text },
+    workspace_id:process.env.WORKSPACE_ID,
+    context,
+  };
+
+  assistant.message(params, (err, response) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json(err);
+    } else {
+      res.json(response);
+    }
+  });
 });
 
 app.listen(port, () => console.log(`Running on port ${port}`));
