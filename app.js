5d82beeb637197667b3802f8c2bdf226e7c63485
Karen Gomes <kngs@ic.ufal.br>
2019-03-23 19:45:03 -0300

    chore(app.js): running prettier

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
 const AssistantV1 = require("watson-developer-cloud/assistant/v1");
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+
 app.use(bodyParser.json());
+app.use(express.static("./public"));
 
 const port = 3000;
 
@@ -12,16 +14,15 @@ const assistant = new AssistantV1({
   username: process.env.ASSISTANT_USERNAME,
   password: process.env.ASSISTANT_PASSWORD,
   url: "https://gateway-wdc.watsonplatform.net/assistant/api",
-  version: "2019-02-23",
-  
+  version: "2019-02-23"
 });
 
 app.post("/conversation/", (req, res) => {
   const { text, context = {} } = req.params;
   const params = {
     input: { text },
-    workspace_id:process.env.WORKSPACE_ID,
-    context,
+    workspace_id: process.env.WORKSPACE_ID,
+    context
   };
 
   assistant.message(params, (err, response) => {
