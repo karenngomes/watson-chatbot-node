d2463285640d9e3c758bfd4362101dd3d4c5ce49
Karen Gomes <kngs@ic.ufal.br>
2019-03-23 19:55:03 -0300

    feat(script.js): getWatsonMessageAndInsertTemplate function

--- public/script.js
+++ public/script.js
@@ -15,3 +15,23 @@ const insertTemplateInChat = template => {
   const chat = document.getElementById("chat");
   chat.appendChild(div);
 };
+
+const getWatsonMessageAndInsertTemplate = async (text = "") => {
+  const uri = "http://localhost:3000/conversation/";
+  const response = await (await fetch(uri, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      text,
+      context
+    })
+  })).json();
+
+  context = response.context;
+
+  const template = chatMessageTemplate(response.output.text, "watson");
+
+  insertTemplateInChat(template);
+};
+
+getWatsonMessageAndInsertTemplate();
